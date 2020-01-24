1abc04026c715e563b117ec7b38510be6910ac80
Aarchu-B <56674217+Aarchu-B@users.noreply.github.com>
2020-01-24 16:26:40 +0545

    pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -10,8 +10,8 @@ exports.getBootcamps = async (req, res, next) => {
     //copy req.query
     const reqQuery = { ...req.query };
 
-    //Array of fields to exclude that i dont want to be matched for filtering
-    const removeFields = ["select",'sort'];
+    //Array of fields to exclude that i dont want to be matched for filtering i.e if we donot exclude sort,select then it will think them as a field and try to match it with the fields that are in our document
+    const removeFields = ["select", "sort", "page", "limit"];
 
     //loop over removeFields and delete them from reqQuery
     removeFields.forEach(value => delete reqQuery[value]);
@@ -40,19 +40,44 @@ exports.getBootcamps = async (req, res, next) => {
     }
     //Sort
     if (req.query.sort) {
-      const sortBy = req.query.sort.split(",").join(" "); query = query.sort(sortBy);
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
     } else {
       query = query.sort("-createdAt");
     }
+    //pagination
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 25;
+    const skip = (page - 1) * limit;
+    const endIndex = page * limit;
+    const total = await bootcamps.countDocuments();
+
+    query = query.skip(skip).limit(limit);
 
     //const bootcamp = await bootcamps.find();
 
     //executing query
     const bootcamp = await query;
 
+    //pagination results
+    const pagination = {};
+    if (endIndex < total) {
+      pagination.next = {
+        page: page + 1,
+        limit: limit
+      };
+    }
+    if (skip > 0) {
+      pagination.prev = {
+        page: page - 1,
+        limit
+      };
+    }
+    console.log(pagination);
     res.status(200).json({
       success: true,
       count: bootcamp.length,
+      pagination,
       data: bootcamp
     });
   } catch (err) {
