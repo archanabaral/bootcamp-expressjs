913807e7fcfc2f75df6bb09ed30b06b4bcc467ab
Aarchu-B <56674217+Aarchu-B@users.noreply.github.com>
2020-01-24 12:55:26 +0545

    impoting from file to DB

---
+++ seeder.js
@@ -0,0 +1,48 @@
+const fs =require('fs');
+const mongoose=require('mongoose');
+const dotenv=require('dotenv');
+
+//load env variables
+dotenv.config({path:'./config/config.env'});
+//load models
+const bootcamp=require('./models/bootcampmodels');
+//connect to DB
+mongoose.connect(process.env.MONGO_URI , {
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useFindAndModify:false,
+    useUnifiedTopology:true
+});
+//Read JSON files
+const bootcamps=JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`,'utf-8'));//utf-8 is encoding type
+
+//Import into db
+
+const importData=async()=>{
+    try{
+        await bootcamp.create(bootcamps);//we dont have to save its result to any variable because we dont need to respond with anything just our goal isto save it to database
+        console.log('Data imported');
+        process.exit();
+
+    }catch(err){
+
+    }
+}
+//Delete data
+const deleteData=async()=>{
+    try{
+        await bootcamp.deleteMany();
+        console.log('Data destroyed');
+        process.exit();
+
+    }catch(err){
+
+    }
+}
+
+if(process.argv[2]==='import'){
+    importData();
+
+}else if(process.argv[2]==='delete'){
+    deleteData();
+}
