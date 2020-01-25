6bd4aa27d20be76b89187a4951545d7d1650c57e
Aarchu-B <archanabaral2000@gmail.com>
2020-01-25 13:27:00 +0545

    creating courses

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require("../utils/errorResponse");
 const Course = require("../models/coursemodels");
+const Bootcamp=require('../models/bootcampmodels');
 
 //route api/v1/courses (this is gonna get all the courses)
 //route GET/api/v1/bootcamps/:bootcampId/courses(this gets all the courses for the specific bootcamp)
@@ -24,4 +25,49 @@ exports.getCourses = async (req, res, next) => {
     next(err);
     console.log(err);
   }
+};
+exports.getCourse=async(req,res,next)=>{
+  try{
+    const course=await Course.findById(req.params.id).populate({
+      path:'bootcamp',
+      select:'name description'
+    });
+    if(!course){
+      return next(new ErrorResponse(`No course with id of ${req.params.id},404`));
+    }
+    res.status(200).json({
+      success:true,
+      data:course
+    });
+
+
+  }catch(err){
+    console.log(err);
+    next(err);
+  }
+
+};
+
+//to create course 
+//route POST/api/v1/bootcamps/:bootcampId/courses
+exports.createCourse=async(req,res,next)=>{
+  try{
+    req.body.bootcamp=req.params.bootcampId;//here bootcamp is refering to bootcamp that is in courses model as field 
+
+    const bootcamp=await Bootcamp.findById(req.params.bootcampId)
+    if(!bootcamp){
+      return next(new ErrorResponse(`No bootcamp with id of ${req.params.bootcampId},404`));
+    }
+    const course=await Course.create(req.body);
+    res.status(200).json({
+      success:true,
+      data:course
+    });
+
+
+  }catch(err){
+    console.log(err);
+    next(err);
+  }
+
 };
--- routes/coursesroute.js
+++ routes/coursesroute.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const CoursesController = require("../controllers/courses");
 
-const router=express.Router({mergeParams:true});//for merging that incoming params to base route i.e :bootcampId/courses to /
+const router = express.Router({ mergeParams: true }); //for merging that incoming params to base route i.e :bootcampId/courses to /
 
-router.route('/').get(CoursesController.getCourses);
-module.exports=router;
+router
+  .route("/")
+  .get(CoursesController.getCourses)
+  .post(CoursesController.createCourse);
+router.route("/:id").get(CoursesController.getCourse);
+
+module.exports = router;
